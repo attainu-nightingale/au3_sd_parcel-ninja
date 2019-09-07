3e18331cfbdab2742f93fd1a3f57215682a2085e
sonam tripathi <mailtosonam123@gmail.com>
2019-09-07 15:48:14 +0530

    Completed client dashboard tried to write in the routes

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express=require("express");
 const app=express();
+const exphbs = require("express-handlebars");
 const home =require("./home");
 const aboutus =require("./aboutus");
 const contactus =require("./contactus");
-const adminlogin =require("./adminlogin");
 const ninjalogin =require("./ninjalogin");
 const clientlogin=require("./clientlogin");
-const adminlanding=require("./adminlanding");
 const clientdashboard=require("./clientdashboard");
 const ninjadashboard=require("./ninjadashboard");
-const ninjabooking=require("./ninjabooking");
+
+const mongoClient = require("mongodb").MongoClient;
+var url = "mongodb://localhost:27017"
+
+mongoClient.connect(url, (err, client) => {
+    if (err) throw err;
+    app.locals.db = client.db("parcelninja");
+  });
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/" ,function(req, res){
     res.send("welcome to homepage")
@@ -18,12 +32,9 @@ app.get("/" ,function(req, res){
 app.use("/home",home);
 app.use("/aboutus",aboutus);
 app.use("/contactus",contactus);
-app.use("/adminlogin",adminlogin);
 app.use("/ninjalogin",ninjalogin);
 app.use("/clientlogin",clientlogin);
-app.use("/adminlanding",adminlanding);
 app.use("/clientdashboard",clientdashboard);
-app.use("/ninjabooking",ninjabooking);
 app.use("/ninjadashboard",ninjadashboard);
 
 app.listen(3000, function(){
